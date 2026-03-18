2dc354b51256110c9c615be0df08ffa19cc9f49d
Maggie Flatt <maggieflatt19@gmail.com>
2026-03-18 17:15:49 -0500

    added expand to retrieve_post

--- views/posts.py
+++ views/posts.py
@@ -35,22 +35,17 @@ def get_posts():
         # Initialize an empty list and then add each dictionary to it
         posts = []
         for row in query_results:
-            user = {
-                "first_name": row['first_name'] ,
-                "last_name": row['last_name']
-            }
-            category = {
-                "label": row['label']
-            }
+            user = {"first_name": row["first_name"], "last_name": row["last_name"]}
+            category = {"label": row["label"]}
             post = {
-                "id": row['post_id'],
+                "id": row["post_id"],
                 "user": user,
                 "category": category,
-                "title": row['title'],
-                "publication_date": row['publication_date'],
-                "image_url": row['image_url'],
-                "content": row['content'],
-                "approved": row['approved']
+                "title": row["title"],
+                "publication_date": row["publication_date"],
+                "image_url": row["image_url"],
+                "content": row["content"],
+                "approved": row["approved"],
             }
             posts.append(post)
 
@@ -59,29 +54,48 @@ def get_posts():
 
     return serialized_posts
 
+
 def retrieve_post(pk):
     """Run query to get a single post and return serialized result"""
     with sqlite3.connect("./db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
+        # Expand user_id to get author name AND expand category_id to get category name
         db_cursor.execute(
             """
             SELECT
-                id,
-                user_id,
-                category_id,
-                title,
-                publication_date,
-                image_url,
-                content,
-                approved
-            FROM Posts
-            WHERE id = ?
+                p.id post_id,
+                p.title,
+                p.publication_date,
+                p.image_url,
+                p.content,
+                p.approved,
+                u.first_name, 
+                u.last_name,
+                c.label 
+            FROM Posts p
+            JOIN Users u ON u.id = p.user_id
+            JOIN Categories c ON c.id = p.category_id
+            WHERE p.id = ?
             """,
             (pk,),
         )
-        row = db_cursor.fetchone()
-        serialized_post = json.dumps(dict(row))
+        query_results = db_cursor.fetchone()
+        print(dict(query_results))
+
+        # Expanded single post
+        row = query_results
+        post = {
+            "id": row["post_id"],
+            "user": {"first_name": row["first_name"], "last_name": row["last_name"]},
+            "category": {"label": row["label"]},
+            "title": row["title"],
+            "publication_date": row["publication_date"],
+            "image_url": row["image_url"],
+            "content": row["content"],
+            "approved": row["approved"],
+        }
+        serialized_post = json.dumps(post)
 
     return serialized_post
