193aa6ad87b7e9d5cc5e1c3a26746c6e8027ced3
Larissa Ferreira <larissadferr@gmail.com>
2026-03-20 13:56:54 -0500

    draft - get comments by post id

--- views/comments.py
+++ views/comments.py
@@ -3,3 +3,15 @@
 # create_comment(comment_data)
 # delete_comment(pk)
 # update_comment(pk, comment_data)
+
+import sqlite3
+import json
+
+
+def get_comments_by_post_id(post_id):
+    """Run query to get all comments by post id"""
+    # open a connection with the database
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        db_cursor.execute("""""")
